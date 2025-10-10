3227617612b6a97e3655b1fb895c4d68a2a0c91a
Subham Kumar Sathua <137478528+SubhamSathua@users.noreply.github.com>
2025-10-10 20:22:55 +0530

    fix

--- FrontendNG/src/app/register/register.ts
+++ FrontendNG/src/app/register/register.ts
@@ -11,6 +11,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './register.html',
   styleUrls: ['./register.css'] 
 })
+
 export class Register {
   registerInput = {
     firstName: '',
